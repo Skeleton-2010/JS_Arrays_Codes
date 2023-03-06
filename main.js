ce99859cad33a1aca0a713fed1549ed8eb756452
Skeleton-2010 <95185689+Skeleton-2010@users.noreply.github.com>
2023-03-06 18:51:21 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("1-st")
+let array = [1, 2, 3, 8, 16, 32, 44, 11, 37]
+console.log(array);
+console.log(array.length);
+
+console.log("2-nd");
+array = ["Anna", "Max", "Isabella", "Andrew", "Lisa", "Alan", "Alice", "Christopher", "Mary", "Sophia"];
+console.log(array)
+console.log(array[0]+", "+array[3]+", "+array[7]+".");
+
+console.log("3-rd");
+
+array = [null, null, null, null, null, null, null, null];
+
+array[0]=10
+console.log(array);
+
+array[2] = 8;
+console.log(array);
+
+array[4] = 100;
+console.log(array);
